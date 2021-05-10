66ad8e1d01adb4995bb1856ddb8f833ac5866ef1
Cargalher <cargalher@gmail.com>
2021-05-10 15:37:45 +0200

    snack1_structure

--- main.js
+++ main.js
@@ -16,3 +16,67 @@ un array e due numeri (“a” più piccolo di “b” e “b” grande al massi
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b” */
 
 // snack 1
+var zucchini = [
+    
+        variety1 = {
+            variety: "Aristocrat",
+            weight: 0.5,
+            length: 7,
+    
+        },
+        variety2 = {
+            variety: "Black Zucchini",
+            weight: 0.6,
+            length: 6.5,
+    
+        },
+        variety3 = {
+            variety: 'Zucchini Gadzukes',
+            weight: 0.6,
+            length: 10,
+    
+        },
+        variety4 = {
+            variety: 'Caserta',
+            weight: 0.7,
+            length: 13,
+    
+        },
+        variety5 = {
+            variety: 'Nimba',
+            weight: 2.5,
+            length: 11,
+    
+        },
+        variety6 = {
+            variety: 'Table Dainty',
+            weight: 0.3,
+            length: 8,
+    
+        },
+        variety7 = {
+            variety: 'Gold Rush',
+            weight: 0.5,
+            length: 7.5,
+    
+        },
+        variety8 = {
+            variety: 'Black Beauty',
+            weight: 0.6,
+            length: 12,
+    
+        },
+        variety9 = {
+            variety: 'Embassy',
+            weight: 0.2,
+            length: 8.5,
+    
+        },
+        variety10 = {
+            variety: 'Greyzini',
+            weight: 0.3,
+            length: 9,
+    
+        },
+    
+    ]
