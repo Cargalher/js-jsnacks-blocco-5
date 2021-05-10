c3ab7aa1da02839fac4117ae029038ba817962d9
Cargalher <cargalher@gmail.com>
2021-05-10 16:23:00 +0200

    snack 4 done_function with for loop

--- main.js
+++ main.js
@@ -19,6 +19,8 @@ Scrivi una funzione che accetti tre argomenti:
 un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo quanto il numero di elementi dell’array).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b” */
 
+
+
 // snack 1
 /*
 var zucchini = [
@@ -96,6 +98,70 @@ var zucchini = [
 /*
 var zucchini = [
     
+        variety1 = {
+            variety: "Aristocrat",
+            weight: 0.5,
+            length: 7,
+    
+        },
+        variety2 = {
+            variety: "Black Zucchini",
+            weight: 0.6,
+            length: 6.5,
+    
+        },
+        variety3 = {
+            variety: 'Zucchini Gadzukes',
+            weight: 0.6,
+            length: 10,
+    
+        },
+        variety4 = {
+            variety: 'Caserta',
+            weight: 0.7,
+            length: 13,
+    
+        },
+        variety5 = {
+            variety: 'Nimba',
+            weight: 2.5,
+            length: 11,
+    
+        },
+        variety6 = {
+            variety: 'Table Dainty',
+            weight: 0.3,
+            length: 8,
+    
+        },
+        variety7 = {
+            variety: 'Gold Rush',
+            weight: 0.5,
+            length: 7.5,
+    
+        },
+        variety8 = {
+            variety: 'Black Beauty',
+            weight: 0.6,
+            length: 12,
+    
+        },
+        variety9 = {
+            variety: 'Embassy',
+            weight: 0.2,
+            length: 8.5,
+    
+        },
+        variety10 = {
+            variety: 'Greyzini',
+            weight: 0.3,
+            length: 9,
+    
+        },
+    
+    ]
+var zucchini = [
+    
         variety1 = {variety: "Aristocrat", weight: 0.5, length: 7,},
         variety2 = {variety: "Black Zucchini", weight: 0.6, length: 6.5,},
         variety3 = {variety: 'Zucchini Gadzukes', weight: 0.6,length: 10,},
@@ -110,9 +176,29 @@ var zucchini = [
 */
 
 // snack 3
-
+/*
 function reverseString (word){
     var reverseWord= word.split ('').reverse().join('');
     console.log(reverseWord);
 }
 reverseString("ciao");
+*/
+
+// snack 4
+
+function elementFusion (array1, array2) {
+    var unionArray = [];
+
+    for (var i =0; i<array1.length ; i++) {
+      unionArray.push(array1[i], array2[i]);
+    }
+    console.log(unionArray);
+    return unionArray;
+}
+
+var arrayA = [1, 2, 3, 4, 5];
+var arrayB = ["a", "b", "c", "d", "e"];
+elementFusion(arrayA, arrayB);
+
+//to have the elements printed in the console i simply console.log
+console.log(elementFusion(arrayA, arrayB));
